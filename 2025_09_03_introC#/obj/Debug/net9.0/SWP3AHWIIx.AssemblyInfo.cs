923871ca19a8af407ab2df3d019033f1aeeb196f
Ben <GRA230096@spengergasse.at>
2025-09-15 14:24:22 +0200

    commit

--- 2025_09_03_introC#/obj/Debug/net9.0/SWP3AHWIIx.AssemblyInfo.cs
+++ 2025_09_03_introC#/obj/Debug/net9.0/SWP3AHWIIx.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SWP3AHWIIx")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e01899bf76bf7225cdadd7c8fc9cf7419c0d68c6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a26be579e8c255ad57390b7d736a20d97f0dbb1")]
 [assembly: System.Reflection.AssemblyProductAttribute("SWP3AHWIIx")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SWP3AHWIIx")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

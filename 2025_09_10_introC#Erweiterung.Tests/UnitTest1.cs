acfe24ef111a3134082453c30654febf08511522
Ben <GRA230096@spengergasse.at>
2025-10-08 10:00:26 +0200

    prisma

--- 2025_09_10_introC#Erweiterung.Tests/UnitTest1.cs
+++ 2025_09_10_introC#Erweiterung.Tests/UnitTest1.cs
@@ -5,6 +5,9 @@ public class UnitTest1
     [Fact]
     public void Test1()
     {
-
+        var frac1 = new Fraction(1, 2);
+        var frac2 = new Fraction(1, 3);
+        var result = frac1 + frac2;
+        Assert.Equal("5/6", result.ToString());
     }
 }

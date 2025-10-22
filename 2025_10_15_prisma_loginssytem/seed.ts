58189da3732319c9a01f4ad40ad4ecbc2173856b
Ben <GRA230096@spengergasse.at>
2025-10-22 10:14:24 +0200

    prisma

---
+++ 2025_10_15_prisma_loginssytem/seed.ts
@@ -0,0 +1,41 @@
+import { PrismaClient } from "./prisma/client/client.ts";
+const prisma = new PrismaClient();
+
+//const class_users = ["Ben", "Georgi", "Marcel", "Dejan", "Moritz"];
+const seed_users = [
+  { name: "Ben", email: "ben@example.com", password: "abc123" },
+  { name: "jerk", email: "jerk@example.com", password: "abc123" },
+  { name: "marcel", email: "marcel@example.com", password: "abc123" },
+  { name: "dejan", email: "dejan@example.com", password: "abc123" },
+  { name: "Georg", email: "Georg@example.com", password: "abc123" },
+];
+
+const seed_posts = [
+  { title: "Hello World", content: "guten Tag", username: "marcel" },
+  { title: "Hello Mars", content: "its cold", username: "Ben" },
+  { title: "Guten Morgen", content: "habe hunger", username: "jerk" },
+  { title: "Guten Abend", content: "habe durst", username: "dejan" },
+  { title: "Guten Nacht", content: "habe schlaf", username: "Georg" },
+];
+
+export async function seed() {
+  for (const user of seed_users) {
+    await prisma.user.create({
+      data: user,
+    });
+  }
+  for (const post of seed_posts) {
+    await prisma.post.create({
+      data: {
+        title: post.title,
+        content: post.content,
+        user: { connect: { name: post.username } },
+      },
+    });
+  }
+}
+
+if (import.meta.main) {
+  await seed();
+  console.log("Seeding finished.");
+}

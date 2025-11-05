c0cac20daa0f155eaefc2b877b0ddd575bed095c
Ben <GRA230096@spengergasse.at>
2025-11-05 11:35:18 +0100

    BrücheohenGPT

---
+++ 2025_11_05_Brueche/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            if (args.Length != 2)
+            {
+                throw new Exception("Bitte genau zwei Brüche als Argumente angeben.");
+            }
+            if (args[0].IndexOf('/') == -1 || args[1].IndexOf('/') == -1)
+            {
+                throw new Exception(
+                    "Die Brüche müssen im Format Zähler/Nenner angegeben werden. z.B. 3/4"
+                );
+            }
+            foreach (var arg in args)
+            {
+                Console.WriteLine(arg);
+            }
+            Bruch bruch1 = new Bruch(args[0]);
+            Bruch bruch2 = new Bruch(args[1]);
+            var ergebnis = bruch1.addiere(bruch2);
+            Console.WriteLine("Ergebnis: " + ergebnis);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
